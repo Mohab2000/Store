da159768c659834913c542e4b6177b871d80784e
Mohab2000 <mohabahmed20005@gmail.com>
2022-12-15 01:09:43 +0200

    add middlewares for logging , security

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
 import express, { Application, Request, Response } from 'express';
-
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 const PORT = 3000;
 const app: Application = express();
+
+//HTTP request logger middleware
+app.use(morgan('common'));
+//middleware to parse incoming requests
+app.use(express.json());
+//HTTP security middleware
+app.use(helmet());
+
+//To avoid spam
+app.use(
+  rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: 'Too many requests from this IP , please try again after an hour.',
+  })
+);
+//Post request
+app.post('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello from post',
+    data: req.body,
+  });
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.json({
